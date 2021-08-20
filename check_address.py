f0536edd7958dff01d9dc2629dbe1a9a25928b13
Tyler <tylercampbell@mac.com>
2021-08-19 20:10:08 -0600

    Minor script update

--- check_address.py
+++ check_address.py
@@ -5,8 +5,11 @@ import threading
 import re
 import datetime
 import os
+import time
+import subprocess
 
-CHECK_INTERVAL = 60 # Time in seconds between IP checks
+CHECK_INTERVAL = 60 # (1m) Time in seconds between IP checks
+ALERT_INTERVAL = 899.5 # (15m) Time in between print statements that IP did not change
 
 # Returns WAN IP as a string. Yeah, it's hacky, but this whole solution is a band-aid anyways
 def getPublicIp():
@@ -15,27 +18,62 @@ def getPublicIp():
 
     return re.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(data).group(1)
 
-myIP = getPublicIp()
+def readIpFromFile():
+    with open('address', 'r') as file:
+        data = file.read()
+        return re.compile(r'http://(\d+\.\d+\.\d+\.\d+):').search(data).group(1)
+
+lastAlert = 0
+myIP = readIpFromFile()
 print("Starting IP Set to: " + myIP)
 
+# Runs a shell command. retval[0] = return code, retval[1] = output, retval[2] = error output
+def runcommand (cmd):
+    proc = subprocess.Popen(cmd,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            shell=True,
+                            universal_newlines=True)
+    std_out, std_err = proc.communicate()
+    return proc.returncode, std_out, std_err
+
+gitPushFailed = False
 def updateGitRepo(newIP):
-    myIP = newIP
-    os.system("echo \"http://" + myIP + ":15015\" > address")
-    os.system("git add address")
-    os.system("git commit -m \"Updated address to " + myIP + "\"")
-    os.system("git push origin master")
+    global myIP
+    global gitPushFailed
+    if gitPushFailed == False or readIpFromFile() != newIP:
+        # If there was a push failure, the commit has already been made but it hasn't been pushed up.
+        print("Making a new commit for the updated IP")
+        os.system("echo \"http://" + newIP + ":15015\" > address")
+        os.system("git add address")
+        os.system("git commit -m \"Updated address to " + newIP + "\"")
+    # Following command may fail if there are network issues. So check for that.
+    pushResult = runcommand("git push origin master")
+    if pushResult[0] == 0:
+        myIP = newIP
+        gitPushFailed  = False
+        print(pushResult[1])
+    else:
+        gitPushFailed = True
+        print("!! Failed to upload git! Will try again next interval !!")
+        print(pushResult[2])
 
 def checkIP():
+    global lastAlert
     threading.Timer(CHECK_INTERVAL, checkIP).start()
 
-    newIP = getPublicIp()
-    if newIP == myIP:
-        print(str(datetime.datetime.now()) + " | IP didn't change.")
-    else:
-        print(str(datetime.datetime.now()) + " | IP HAS CHANGED!!")
-        print("  OLD IP: " + myIP)
-        print("  NEW IP: " + newIP)
-        updateGitRepo(newIP)
+    try:
+        newIP = getPublicIp()
+        if newIP == myIP:
+            if (time.time() - lastAlert) >= ALERT_INTERVAL:
+                lastAlert = time.time()
+                print(str(datetime.datetime.now()) + " | IP didn't change.")
+        else:
+            print(str(datetime.datetime.now()) + " | IP HAS CHANGED!!")
+            print("  OLD IP: " + myIP)
+            print("  NEW IP: " + newIP)
+            updateGitRepo(newIP)
+    except Exception as error:
+        print("Failed to get IP.\n" + str(error))
 
-
-threading.Timer(CHECK_INTERVAL, checkIP).start()
+checkIP()
